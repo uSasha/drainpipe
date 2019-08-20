4e493f49db94f55b4cd231218676528ed0ac8d4f
uSasha <sasha.engineer@gmail.com>
2019-08-20 12:57:56 +0300

    use consumer groups for scalability

--- src/drainpipe.py
+++ src/drainpipe.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import time
 
@@ -9,11 +10,13 @@ help_message = 'Hello, please pass pattern to match and path to CSV file to dump
 
 
 class StreamDumper:
-    def __init__(self, redis, pattern, log_path):
+    def __init__(self, redis, pattern, log_path, consumer_group='default', consumer_name='default'):
         self.redis = redis
         self.pattern = pattern
         self.log_path = log_path
         self.stream_cursor = dict()
+        self.consumer_group = consumer_group
+        self.consumer_name = consumer_name
 
         try:
             with open(self.log_path, 'r') as f:
@@ -30,12 +33,18 @@ class StreamDumper:
             return []
 
     def consume_streams(self):
-        _, streams = cache.scan(match=self.pattern, count=int(10e10))
+        _, streams = self.redis.scan(match=self.pattern, count=int(10e10))  # somehow None option don't work
         for stream in streams:
             if stream not in self.stream_cursor:
-                self.stream_cursor[stream] = self.redis.xrevrange(stream, count=1)[0][0]  # last ID
+                try:
+                    self.stream_cursor[stream] = '>'
+                    self.redis.xgroup_create(stream, self.consumer_group)
+                except redis.exceptions.ResponseError:
+                    pass
+        if not self.stream_cursor:
+            return
 
-        result = cache.xread(self.stream_cursor, block=None)
+        result = self.redis.xreadgroup(self.consumer_group, self.consumer_name, self.stream_cursor, noack=True)
         if not self.header and result:
             self.header = self.find_header(result)
             with open(self.log_path, 'w') as f:
@@ -48,10 +57,9 @@ class StreamDumper:
 
             for column in self.header:
                 if column not in ['stream', 'timestamp']:
-                    df[column] = df['content'].apply(lambda c: c.get(column.encode()).decode())
+                    df[column] = df['content'].apply(lambda c: c.get(column.encode(), b'').decode())
 
             df[self.header].to_csv(self.log_path, mode='a', index=False, header=None)
-            self.stream_cursor[stream] = result[-1][1][-1][0]
 
 
 if __name__ == '__main__':
@@ -64,9 +72,13 @@ if __name__ == '__main__':
     redis_host = os.environ.get('redis_host') or 'localhost'
     redis_port = int(os.environ.get('redis_port') or 6379)
     idle_seconds = float(os.environ.get('idle_seconds') or 1)
+    consumer_group = os.environ.get('consumer_group') or 'drainpipe'
+    consumer_name = os.environ.get('HOSTNAME') or 'local'
+    if 'linuxkit' in consumer_name:
+        consumer_name = random.randint(0, 10e10)  # docker for mac
 
     cache = redis.Redis(redis_host, redis_port)
-    drain = StreamDumper(cache, pattern, path_to_csv)
+    drain = StreamDumper(cache, pattern, path_to_csv, consumer_group, consumer_name)
 
     while True:
         drain.consume_streams()
